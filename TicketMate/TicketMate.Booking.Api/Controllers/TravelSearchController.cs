d7722f672a7f0ce86ee979ae7092132c98ae4275
DilshanPathegamage123 <141545307+DilshanPathegamage123@users.noreply.github.com>
2024-04-08 19:27:54 +0530

    Commit 2

---
+++ TicketMate/TicketMate.Booking.Api/Controllers/TravelSearchController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TicketMate.Booking.Application.Handlers;
+using TicketMate.Booking.Application.Services;
+using TicketMate.Booking.Infrastructure;
+
+namespace TicketMate.Booking.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TravelSearchController : ControllerBase
+    {
+
+        private readonly TravelSearchHandler _travelSearchHandler;
+
+
+        public TravelSearchController(TravelSearchHandler travelSearchHandler)
+        {
+
+            _travelSearchHandler = travelSearchHandler;
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddTravelSearch([FromBody] TicketMate.Booking.Api.Models.TravelSearch inputTravelSearch)
+        {
+            try
+            {
+
+                var newTravelSearch = await _travelSearchHandler.AddTravelSearch(inputTravelSearch);
+
+
+
+                // After saving the new travel search, automatically redirect to the GET API with the same parameters
+                return RedirectToAction("GetTravelSearchResults", new
+                {
+                    vehicleType = newTravelSearch.VehicleType,
+                    startLocation = newTravelSearch.StartLocation,
+                    endLocation = newTravelSearch.EndLocation,
+                    travelDate = newTravelSearch.TravelDate
+                }
+
+                    );
+
+                // return Ok(newTravelSearch);
+
+            }
+            
+                
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+
+        }
+
+
+        [HttpGet]
+        public IActionResult GetTravelSearchResults([FromQuery] string vehicleType, [FromQuery] string startLocation, [FromQuery] string endLocation, [FromQuery] string travelDate)
+        {
+            try
+            {
+                // Retrieve search results based on vehicle type, start location, end location, and travel date
+                var searchResults = _travelSearchHandler.GetTravelSearchResults(vehicleType, startLocation, endLocation, travelDate);
+                    
+
+                return Ok(searchResults);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ TicketMate/TicketMate.Booking.Application/Handlers/TravelSearchHandler.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketMate.Booking.Api.Models;
+using TicketMate.Booking.Infrastructure;
+
+namespace TicketMate.Booking.Application.Handlers
+{
+    public class TravelSearchHandler
+    {
+        private readonly BookingDbContext _dbContext;
+
+        public TravelSearchHandler(BookingDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<TravelSearch> AddTravelSearch(TicketMate.Booking.Api.Models.TravelSearch inputTravelSearch)
+        {
+            try
+            {
+                var newTravelSearch = new TicketMate.Booking.Api.Models.TravelSearch
+                {
+                    VehicleType = inputTravelSearch.VehicleType,
+                    StartLocation = inputTravelSearch.StartLocation,
+                    EndLocation = inputTravelSearch.EndLocation,
+                    TravelDate = inputTravelSearch.TravelDate
+                };
+
+                _dbContext.TravelSearch.Add(newTravelSearch);
+                await _dbContext.SaveChangesAsync();
+
+                return newTravelSearch;
+            }
+            catch (DbUpdateException ex)
+            {
+                Console.WriteLine(ex);
+                var errorMessage = ex.InnerException != null ? ex.InnerException.Message : ex.Message;
+                throw new Exception ($"Error while saving changes: {errorMessage}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception (ex.Message);
+            }
+        }
+
+        public IEnumerable<TravelSessions> GetTravelSearchResults(string vehicleType, string startLocation, string endLocation, string travelDate)
+        {
+            try
+            {
+                var searchResults = _dbContext.TravelSessions
+                    .Where(ts => ts.VehicleType == vehicleType && ts.StartLocation == startLocation && ts.EndLocation == endLocation && ts.TravelDate == travelDate)
+                    .ToList();
+
+                return searchResults;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error while retrieving travel search results: {ex.Message}");
+            }
+        }
+
+        public IEnumerable<TravelSessions> GetAllTravelSearchResults()
+        {
+            try
+            {
+                var allSearchResults = _dbContext.TravelSessions.ToList();
+                return allSearchResults;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error while retrieving all travel search results: {ex.Message}");
+            }
+        }
+    }
+}
