4bf72dcad8a024655f1659422c952e8b03117e1d
Himasha Sandeepani <141569662+SANDEEPANI-RMH@users.noreply.github.com>
2024-04-18 12:24:46 +0530

    email sending

---
+++ TicketMate/TicketMate.Payment.Domain/DTOs/RequestDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketMate.Payment.DTOs
+{
+    public class RequestDTO
+    {
+        public string To { get; set; } = string.Empty;
+        public string Subject { get; }
+        public string Message { get; set; } = string.Empty;
+
+
+        // Constructor to set default value for Subject
+        public RequestDTO()
+        {
+
+       
+            Subject = "Your payment receipt"; // Set default value for Subject
+
+        }
+    }
+}
+
